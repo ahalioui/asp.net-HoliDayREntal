aa7b9b10449c0d7f2eaa73f8611634de70f33f5a
Assmaa <a.halioui@interface3.be>
2022-02-22 10:55:33 +0100

    Debut epreuve

---
+++ HoliDayRental.DAL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HoliDayRental.DAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
